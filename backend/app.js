a071e0d9ec4c067cc8139bedf7288c94b330f441
Mindi Rebeka <168852888+Mindi-R@users.noreply.github.com>
2025-04-09 16:56:55 +0530

    Add database and middleware

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,35 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './config/env.js';
+import errorMiddleware from './middleware/error.middleware.js';
 
 import authRouter from './routes/auth.routes.js';
 import hostRouter from './routes/host.routes.js';
 import boardingRouter from './routes/boarding.routes.js';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/host', hostRouter);
-app.use('/api/v1/boarding', boardingRouter);
+app.use('/api/v1/hosts', hostRouter);
+app.use('/api/v1/boardings', boardingRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req,res) => {
     res.send('Hello World');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
 
 export default app;
+
--- backend/config/env.js
+++ backend/config/env.js
@@ -4,4 +4,9 @@ import { config } from 'dotenv';
 
 config({path: `.env.${process.env.NODE_ENV||'development'}.local`});
 
-export const { PORT , NODE_ENV } = process.env;
+export const { PORT , 
+               NODE_ENV , 
+               DB_URI ,
+               CLOUD_NAME ,
+               API_KEY ,
+               API_SECRET} = process.env;
